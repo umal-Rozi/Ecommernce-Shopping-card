592a9c03cf957b69498a819a50eff7e6ae4aff35
umalrozi <umalrozi@email.com>
2023-03-13 13:41:15 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Header from "./components/Header";
+
 const App =() =>{
     return(
-        <h1> Hello World!</h1>
+        <div>
+            <Header/>
+        </div>
     );
 };
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () =>{
+
+    return (
+        <div className="Menu">
+        <a href=""> logo</a>
+        <ul>
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">About</a>
+            </li>
+            <li>
+                <a href="">Contact</a>
+            </li>
+            <li>
+                <a href="">Cart</a>
+            </li>
+        </ul>
+        <a href="">5</a>
+    </div>
+    );   
+};
+
+export default Header;
